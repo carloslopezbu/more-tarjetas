dff6e42a13e2db90fa0597ab64fdfbd63f02ef9d
Carlos David López Hinojosa <backupcarlos18@gmail.com>
2025-04-07 02:27:16 +0200

    sss

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -9,6 +9,7 @@ import { createClient } from "@supabase/supabase-js"
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE
 const supabase = createClient(supabaseUrl, supabaseKey)
+console.log("Supabase Client:")
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase URL or Key in environment variables.")
